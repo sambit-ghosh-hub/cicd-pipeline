cc225da3bb41badeb5db7c3780eb5421c03952d2
sambit-ghosh-hub <sambit-ghosh@outlook.com>
2022-12-18 15:15:04 +0530

    more commands update 5

--- pythonscripts/cicdscript.py
+++ pythonscripts/cicdscript.py
@@ -34,7 +34,7 @@ if timediffmins < 5:
  if platform.system() == 'Windows':
   os.system(".\batchscripts\pullanddeploy.bat")
  else:
-  os.system('cd "$(dirname "$0")";./pullanddeploy.sh')
+  os.system('cd "$(dirname "$0")";cat pullanddeploy.sh')
  
 else:
  print("No new Commits in last 5 mins")
